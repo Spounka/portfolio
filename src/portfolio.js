b5d1125f9ead2426e94b26f3847e2c90e10f86ea
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2026-03-14 00:51:48 +0100

    chore: update url

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,6 +1,6 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://nazih.me/',
+  homepage: 'https://portfolio.nazih.top/',
   title: 'Nazih B.',
 }
 
